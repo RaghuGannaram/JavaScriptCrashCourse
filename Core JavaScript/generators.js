4f1033173bf4d4017ce6dba3d8915d087e4487d6
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-01-25 16:58:29 +0530

    Updated Generators

--- Core JavaScript/generators.js
+++ Core JavaScript/generators.js
@@ -1,64 +1,10 @@
-let dayArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-function* iterateArr(arr) {
-	for (let i in arr) {
-		yield arr[i];
-	}
-	return "*completed*";
-}
-
-let iterateDayArr = iterateArr(dayArr);
-
-console.log(iterateDayArr);
-console.log(iterateDayArr.next());
-console.log(iterateDayArr.next());
-console.log(iterateDayArr.next());
-console.log(iterateDayArr.next());
-console.log(iterateDayArr.next());
-console.log(iterateDayArr.next());
-console.log(iterateDayArr.next());
-console.log(iterateDayArr.next());
-console.log(iterateDayArr.next());
-
-let dayObj = {
-    0: "Sunday",
-    1: "Monday", 
-    2: "Tuesday", 
-    3: "Wednesday", 
-    4: "Thursday", 
-    5: "Friday", 
-    6: "Saturday"
-}
-
-function* iterateObj(obj) {
-	for (let i in obj) {
-		yield obj[i];
-	}
-	return "*completed*";
-}
-
-let iterateDayObj = iterateObj(dayObj);
-
-console.log(iterateDayObj);
-console.log(iterateDayObj.next());
-console.log(iterateDayObj.next());
-console.log(iterateDayObj.next());
-console.log(iterateDayObj.next());
-console.log(iterateDayObj.next());
-console.log(iterateDayObj.next());
-console.log(iterateDayObj.next());
-console.log(iterateDayObj.next());
-console.log(iterateDayObj.next());
-
-
-
-
+//Simple Generator
 function* simpleGenerator() {
-	//    console.log(yield 1)
-	//    console.log(yield 2)
-	//    console.log(yield 3)
+	//  console.log(yield "a")
+	//  console.log(yield "b")
+	//  console.log(yield "c")
+	yield 1;
 	yield 2;
-	yield 3;
 }
 
 let customSeries = simpleGenerator();
@@ -66,20 +12,26 @@ console.log(customSeries.next());
 // console.log(customSeries.next().value)
 console.log(customSeries.next());
 // console.log(customSeries.next().value)
+console.log(customSeries.next());
+// console.log(customSeries.next().value)
 
-// console.log(customSeries.next("a").value)
-// console.log(customSeries.next("b").value)
-// console.log(customSeries.next("c").value)
+// console.log(customSeries.next("x").value)
+// console.log(customSeries.next("y").value)
+// console.log(customSeries.next("x").value)
 // console.log(customSeries.next())
 // console.log(customSeries.next())
 // console.log(customSeries.next())
 
+//ID Generator
 function* generateId() {
 	let id = 1000;
 	while (true) {
-		let init = yield id;
-		if (init) id += init;
-		else id++;
+		let skip = yield id;
+		if (skip) {
+			id += skip;
+		} else {
+			id++;
+		}
 	}
 }
 
@@ -93,3 +45,84 @@ console.log(uniqueId.return(10000));
 console.log(uniqueId.next());
 // console.log(uniqueId.throw(new Error("Hello World")))
 console.log(uniqueId.next());
+
+//Fibonacci Generator
+function* fibonacci() {
+	let [current, next] = [0, 1];
+	while (true) {
+		const reset = yield current;
+		if (reset) {
+			current = 0;
+			next = 1;
+		} else {
+			[current, next] = [next, next + current];
+		}
+	}
+}
+
+const sequence = fibonacci();
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next(true).value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+
+//-------------------------------------------------------------------------------------------------
+
+let daysArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function* daysGenereratorA(arr) {
+	for (let i in arr) {
+		yield arr[i];
+	}
+	return "------";
+}
+
+let daysIteratorA = daysGenereratorA(daysArr);
+
+console.log(daysIteratorA);
+console.log(daysIteratorA.next());
+console.log(daysIteratorA.next());
+console.log(daysIteratorA.next());
+console.log(daysIteratorA.next());
+console.log(daysIteratorA.next());
+console.log(daysIteratorA.next());
+console.log(daysIteratorA.next());
+console.log(daysIteratorA.next());
+console.log(daysIteratorA.next());
+
+let daysObj = {
+	0: "Sunday",
+	1: "Monday",
+	2: "Tuesday",
+	3: "Wednesday",
+	4: "Thursday",
+	5: "Friday",
+	6: "Saturday",
+};
+
+function* daysGenereratorO(obj) {
+	for (let i in obj) {
+		yield obj[i];
+	}
+	return "------";
+}
+
+let daysIteratorO = daysGenereratorO(daysObj);
+
+console.log(daysIteratorO);
+console.log(daysIteratorO.next());
+console.log(daysIteratorO.next());
+console.log(daysIteratorO.next());
+console.log(daysIteratorO.next());
+console.log(daysIteratorO.next());
+console.log(daysIteratorO.next());
+console.log(daysIteratorO.next());
+console.log(daysIteratorO.next());
+console.log(daysIteratorO.next());
