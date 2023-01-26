17ef498413e8b532693f11e5497e6600085e15be
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-01-26 07:39:33 +0530

    Added leetcode-0128 Longest consecutive sequence

---
+++ LeetCode/Arrays & Hashing/0128-longest-consecutive-sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var longestConsecutiveSequence = function (nums) {
+	let currentMax,
+		max = 0;
+	set = new Set(nums);
+
+	for (let i = 0; i < nums.length; i++) {
+		if (set.has(nums[i] - 1)) {
+			continue;
+		}
+		currentMax = 1;
+		while (set.has(nums[i] + currentMax)) {
+			currentMax += 1;
+		}
+		max = Math.max(max, currentMax);
+	}
+	return max;
+};
+
+console.log(longestConsecutiveSequence([100, 4, 200, 1, 3, 2]));
