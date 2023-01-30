6838451c643061fc2156a54a33d4849babdd879b
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-01-30 12:13:50 +0530

    Updated Leetcode-Sliding window

--- LeetCode/Sliding Window/0003-longest-sub-string-without-repeatition.js
+++ LeetCode/Sliding Window/0003-longest-sub-string-without-repeatition.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 
-var lengthOfLongestSubstring = function (s) {
+var lengthOfLongestSubstring1 = function (s) {
 	const set = new Set();
 	let l = 0;
 	let max = 0;
@@ -20,4 +20,40 @@ var lengthOfLongestSubstring = function (s) {
 	return max;
 };
 
-console.log(lengthOfLongestSubstring("abcb"));
+var lengthOfLongestSubstring2 = function (s) {
+	let l = 0,
+		r = 0,
+		max = 0,
+		set = new Set();
+
+	while (r < s.length) {
+		while (set.has(s[r])) {
+			set.delete(s[l]);
+			l++;
+		}
+		set.add(s[r]);
+		max = Math.max(max, set.size);
+		r++;
+	}
+	return max;
+};
+
+var lengthOfLongestSubstring3 = function (s) {
+	let l = 0,
+		r = 0,
+		max = 0,
+		str = "";
+
+	while (r < s.length) {
+		if (str.includes(s[r])) {
+			str = str.slice(str.indexOf(s[r])+1);
+			l = r;
+		}
+		str = str + s[r];
+		max = Math.max(max, str.length);
+		r++;
+	}
+	return max;
+};
+
+console.log(lengthOfLongestSubstring3("aab"));
