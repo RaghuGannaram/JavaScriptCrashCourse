1d8ffac69723b15e0111cbb9e1f05ca0e1c69ebc
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-02-05 18:36:19 +0530

    Update CoreJavaScript/function file

--- Core JavaScript/function.js
+++ Core JavaScript/function.js
@@ -1,93 +1,162 @@
-// Function Statement
-console.log(one);
-one();
-function one() {
-  console.log("function one");
+//-----------------------------------------------Function Statement-----------------------------------------
+function funcStmt(name) {
+	return `Hello ${name}..! Executed by ${funcStmt.name}`;
 }
 
-// Function Expression with Anonymus function
-console.log(two);
-// two()
-var two = function () {
-  console.log("function two");
+console.log(funcStmt("JOhn Doe"));
+
+//-----------------------------------------------Function Expression-----------------------------------------
+var funcExpr = function (name) {
+	return `Hello ${name}..! Executed by ${funcExpr.name}`;
 };
 
-//Function Expression with Arrow function
-console.log(three);
-// three()
-var three = () => {
-  console.log("function three");
+console.log(funcExpr("John Doe"));
+
+//--------------------------------------Function Expression with Arrow functions---------------------------
+var funcExprArr = (name) => {
+	return `Hello ${name}..! Executed by ${funcExprArr.name}`;
 };
 
-//Function Expression with Named function
-console.log(four);
-// four()
-var four = function fourName() {
-  console.log(fourName);
-  return "function four";
-};
-console.log(four);
-console.log(four());
-// console.log(fourName)
-// console.log(fourName());
+console.log(funcExprArr("John Doe"));
 
-//Anonymus function
-// console.log(five)
-// five()
-// (function five() {
-//   console.log("Immediate Invocable Function");
-// })()
-(function funcOne() {
-  console.log("Hello from funcOne ---|", this);
+//----------------------------------------------Named Function Expression-----------------------------------
+var namedFuncExpr1 = function funcName(name) {
+	return `Hello ${name}..! Executed by ${namedFuncExpr1.name}`;
+};
+
+console.log(namedFuncExpr1("John Doe"));
+
+try {
+	console.log(funcName("John Doe"));
+} catch (err) {
+	console.log(err);
+}
+
+//----------------------------------------------Anonymus Function-----------------------------------------
+//Results in Syntax error | No use of try catch...
+try {
+	// function (){
+	// 	return `Hello world`;
+	// }
+} catch (err) {
+	// console.log(err)
+}
+
+//---------------------------------------Immediatey Invokable Function Expression---------------------------
+(function () {
+	console.log(`Hello world...!`);
 })();
 
-let context = { parent: "context" };
+(function (name) {
+	console.log(`Hello ${name}...!`);
+})("John Doe");
 
-context.funcTwo = function () {
-  this.name = "funcTwo";
-  console.log("Hello from funcTwo ---|", this);
+//-----------------------------------------------Function Properies-----------------------------------------
+
+function funcProp(param1, param2, param3, ...restParams) {
+	let a = param1,
+		b = param2,
+		c = param3;
+	d = restParams;
+
+	console.log("Type of arguments", typeof arguments);
+	console.log(`Listof argumets passed, ${[...arguments]}`);
+
+	console.log(`Name of the function : ${funcProp.name}`);
+	console.log(`Arguments of the function : ${[...funcProp.arguments]}`);
+	console.log(`Length of function arguments : ${funcProp.length}`);
+	console.log(`Calling function : ${funcProp.caller.name}`);
+	console.log(`Calling calling function : ${funcProp.caller.caller.name}`);
+}
+
+function callerFunc() {
+	funcProp(1, 2, 3, 4, 5);
+}
+callerFunc();
+
+//------------------------------------------Function.prototype.toString()--------------------------------------------
+var funcProto = function () {
+	console.log("Hello world...!");
 };
-context.funcTwo();
+console.log(funcProto.toString());
 
-function FuncThree() {
-  this.name = "funcThree";
-  console.log("Hello from funcThree ---|", this);
+//-----------------------------------------------Function as Object-----------------------------------------
+
+function funcObj(param1, param2, param3) {}
+
+console.log(funcObj);
+
+funcObj.a = 1;
+funcObj.b = 2;
+funcObj.c = 3;
+
+funcObj.printProps = function () {
+	return `a: ${this.a} | b:${this.b} | c: ${this.c}`;
+};
+
+console.log(funcObj);
+console.log(funcObj.name);
+console.log(funcObj.length);
+console.log(funcObj.prototype);
+
+console.log(funcObj.printProps());
+console.log(funcObj.toString());
+
+//-----------------------------------------------Function on a context-----------------------------------------
+const context = {
+	firstName: "John",
+	lastName: "Doe",
+};
+
+context.info = function () {
+	return `First name: ${this.firstName} | Last name : ${this.lastName}`;
+};
+console.log(context.info());
+
+function greet() {
+	return `Hello ${this.firstName} ${this.lastName}...!`;
 }
-let myfunc = new FuncThree();
-console.log(myfunc);
-console.log(myfunc.__proto__);
-console.log(myfunc.__proto__.constructor);
-console.log(myfunc.__proto__.__proto__ === Object.prototype);
+console.log(greet.call(context));
 
+//-----------------------------------------------Once Function-----------------------------------------
+function once(func) {
+	let called = false;
 
-function testFunction(a, ...params) {
-  console.log(params)
-  console.log(arguments)
-}
-
-testFunction(10, 20, 30)
-
-
-
-//Once function
-
-function once(fn, context) { 
-	var result;
-
-	return function() { 
-		if(fn) {
-			result = fn.apply(context || this, arguments);
-			fn = null;
+	return function () {
+		if (!called) {
+			called = true;
+			return func();
 		}
-
-		return result;
+		return;
 	};
 }
 
-// Usage
-var canOnlyFireOnce = once(function() {
-	console.log('Fired!');
+var greetOnce = once(function () {
+	return `Greeting only once...!`;
 });
 
-canOnlyFireOnce(); // "Fired!"
-canOnlyFireOnce(); // nada
+console.log(greetOnce());
+console.log(greetOnce());
+console.log(greetOnce());
+
+//-----------------------------------------------Nth time callable Function-----------------------------------------
+function NthTimeCallable(func, n){
+	let count = 0;
+
+	return function(){
+		if(count<n){
+			count++;
+			return func();
+		}
+		return `${func.name} has already been called ${count} times...!`
+	}
+}
+
+var greetThreePeople = NthTimeCallable(function Greet(){
+	return `Hello guys...!`;
+}, 3)
+
+console.log(greetThreePeople())
+console.log(greetThreePeople())
+console.log(greetThreePeople())
+console.log(greetThreePeople())
