b82f495ca668f101d5332ca4b8bb128b7f3fd5c4
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-03-08 22:28:29 +0530

    Updated Core JavaScript/map

--- Core JavaScript/map.js
+++ Core JavaScript/map.js
@@ -1,61 +1,76 @@
 let map = new Map();
 
-console.log(map)
-console.log(typeof(map))
-console.log(map instanceof Object)
-console.log(map instanceof Map)
-console.log(map instanceof Array)
+console.log(map);
+console.log(typeof map);
+console.log(map instanceof Object);
+console.log(map instanceof Map);
+console.log(map instanceof Array);
 
+map.set(true, "boolValue");
+console.log(map);
 
-map.set(true, "boolValue")
-console.log(map)
+map.set(1, "numValue");
+console.log(map);
 
-map.set(1, "numValue")
-console.log(map)
+map.set("strKey", "strValue");
+console.log(map);
 
-map.set("strKey", "strValue")
-console.log(map)
+map.set([100, 200], "arrValue");
+console.log(map);
 
-map.set([100, 200], "arrValue")
-console.log(map)
+map.set({ property: true }, "objValue");
+console.log(map);
 
-map.set({property : true}, "objValue")
-console.log(map)
+map.set(2, "twoValue");
+console.log(map);
+console.log(map.size);
+console.log(map.has(2));
+console.log(map.get(2));
 
-map.set(2, "twoValue")
-console.log(map.get(2))
-console.log(map.has(2))
 map.delete(2);
-console.log(map.has(2))
 
-console.log(map.size)
-// for (let i in map.keys()) console.log(i)
-for (let i of map.keys()) console.log(i)
-for (let i of map.keys()) console.log(typeof(i))
-for (let i of map.keys()) console.log(i instanceof Array)
-for (let i of map.keys()) console.log(i instanceof Object)
-// for (let i in map.values()) console.log(i)
-// for (let [i,j] in map.entries()) console.log(i, j)
-for (let [i,j] of map.entries()) console.log(i, j)
+console.log(map);
+console.log(map.size);
+console.log(map.has(2));
+console.log(map.get(2));
 
-console.log(map.size)
+for (let i of map) console.log(i);
+for (let [i, j] of map) console.log(i, j);
 
-let mapTwo = new Map([[10],[20],[40],[50], [30], [30]])
-let mapThree = new Map([[10,"a"],[20, "b"],[40, "c"],[50, "d"], [30, "e"], [30, "f"]])
-// let mapFour = new Map({10:"a",20: "b",40: "c",50: "d", 30: "e", 30: "f"}) //Throws error: Not iterable
+for (let i of map.keys()) console.log(i);
+for (let i of map.values()) console.log(i);
+for (let [i, j] of map.entries()) console.log(i, j);
 
-console.log(mapTwo)
-console.log(mapThree)
-console.log(mapThree.has(60))
+for (let i of map.keys()) console.log(typeof i);
+for (let i of map.keys()) console.log(i instanceof Array);
+for (let i of map.keys()) console.log(i instanceof Object);
 
-const first = new Map([
-  [1, 'one'],
-  [2, 'two'],
-  [3, 'three'],
-  [3, 'thre'],
+try {
+	new Map([10, 20, 30, 40, 50]);
+} catch (error) {
+	console.log(error);
+}
+
+try {
+	new Map({ 10: "a", 20: "b", 40: "c", 50: "d", 30: "e", 30: "f" });
+} catch (error) {
+	console.log(error);
+}
+
+let mapTwo = new Map([[10], [20], [40], [50], [30], [30]]);
+for (let i of mapTwo.keys()) console.log(i);
+for (let i of mapTwo.values()) console.log(i);
+
+let mapThree = new Map([
+	[10, "a"],
+	[20, "b"],
+	[40, "c"],
+	[50, "d"],
+	[30, "e"],
+	[30, "f"],
 ]);
+for (let i of mapThree.keys()) console.log(i);
+for (let i of mapThree.values()) console.log(i);
 
-console.log(first)
-
-
-
+let mapFour = new Map([{ 10: "a" }]);
+console.log(mapFour);
