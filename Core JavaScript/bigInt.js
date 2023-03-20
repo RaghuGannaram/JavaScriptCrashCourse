cfad1fddfc60af22569616b3b22b52b8d797a908
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-03-20 18:06:12 +0530

    Updated Core JavaScript/BigInt

--- Core JavaScript/bigInt.js
+++ Core JavaScript/bigInt.js
@@ -1,8 +1,10 @@
+console.log(BigInt(99999999999999999999999999999999999999999999999999999999999999999999999));
+console.log(BigInt("99999999999999999999999999999999999999999999999999999999999999999999999"));
 let b = BigInt(5);
 console.log(b);
 console.log(typeof b);
 
-let n1 = 9999999999999999999999999999999999999999999999999999999;
+let n1 = 999999999999999;
 let n2 = 9999999999999999999999999999999999999999999999999999999;
 let b1 = 9999999999999999999999999999999999999999999999999999999n;
 let b2 = 9999999999999999999999999999999999999999999999999999999n;
@@ -14,7 +16,7 @@ console.log((b1 * b2).toString().length);
 try {
 	console.log(BigInt());
 	console.log(BigInt(undefined));
-  console.log(BigInt(null));
+	console.log(BigInt(null));
 } catch (error) {
 	console.log(error);
 }
@@ -34,3 +36,23 @@ console.log(BigInt([5]));
 // console.log(BigInt(-Infinity));
 // console.log(BigInt("Infinity"));
 // console.log(BigInt("-Infinity"));
+
+const mixed = [4n, 6, -12n, 10, 4, 0, 0n];
+// [4n, 6, -12n, 10, 4, 0, 0n]
+console.log(mixed)
+mixed.sort(); // default sorting behavior
+// [ -12n, 0, 0n, 10, 4n, 4, 6 ]
+console.log(mixed)
+try {
+	mixed.sort((a, b) => a - b);
+} catch (error) {
+	console.log(error);
+}
+console.log(mixed)
+// won't work since subtraction will not work with mixed types
+// TypeError: can't convert BigInt value to Number value
+
+// sort with an appropriate numeric comparator
+mixed.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+console.log(mixed)
+// [ -12n, 0, 0n, 4n, 4, 6, 10 ]
