f78f1a89771211f29149a885bc0647fc096a4710
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-03-29 21:58:52 +0530

    Updated Core JavaScript/symbols.js

--- Core JavaScript/symbols.js
+++ Core JavaScript/symbols.js
@@ -7,16 +7,21 @@ console.log(s2 == s3);
 
 let sbl1 = Symbol.for("kites");
 let sbl2 = Symbol.for("kites");
+let a = Symbol();
+console.log(typeof a);
+console.log(typeof String(a));
 
 console.log(sbl1 === sbl2);
 
 console.log(Symbol.keyFor(sbl1));
+console.log(Symbol.keyFor(sbl2));
+
+console.log(Symbol.keyFor(Symbol.for("Hello World..!")));
 
 let obj = {
 	a: "a-value",
 	[Symbol.for("b")]: "b-value",
 };
-
 obj["c"] = "c-value";
 obj[Symbol.for("d")] = "d-value";
 
@@ -28,5 +33,10 @@ for (let i in obj) {
 	console.log(obj[i]);
 }
 
-console.log(Object.getOwnPropertyNames(obj))
-console.log(Object.getOwnPropertySymbols(obj))
+console.log(Object.getOwnPropertyNames(obj));
+console.log(Object.getOwnPropertySymbols(obj));
+console.log(Reflect.ownKeys(obj));
+
+console.log(Symbol.keyFor(Symbol.iterator));
+console.log(Symbol);
+console.log(Symbol.prototype);
