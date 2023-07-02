d93a3a4120ec3b952ee548a36d853d038729b4bb
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-07-02 22:35:17 +0530

    Updated Core JavaScript/currying

--- Core JavaScript/currying.js
+++ Core JavaScript/currying.js
@@ -1,28 +1,45 @@
-let add1 = x => x+1;
-let mul2 = x => x*2;
-let div4 = x => x/4;
-
-
-function curryArithmetic(operatorChain){
-    return function(...operators){
-        return function(value){
-            return operatorChain([...operators], value);
-        }
-    }
+function curry(f) {
+	return function (a) {
+		return function (b) {
+			return f(a, b);
+		};
+	};
 }
 
-function operatorChain(operators, value){
-    let chainedOperator =value
-    operators.forEach(operator=>{
-        chainedOperator =operator(chainedOperator);
-    })
-    return chainedOperator;
+function sum(a, b) {
+	return a + b;
 }
 
-let pipe = curryArithmetic(operatorChain)
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1)(2));
 
 
-console.log("Expected 2, Result :",pipe(add1, mul2, div4)(3))
-console.log("Expected 1.75, Result :",pipe(mul2, add1, div4)(3))
-console.log("Expected 2.5, Result :",pipe(mul2, div4, add1)(3))
-console.log("Expected 5, Result :",pipe(mul2, div4, add1, mul2)(3))
+let add1 = (x) => x + 1;
+let mul2 = (x) => x * 2;
+let div4 = (x) => x / 4;
+
+function curryArithmetic(applyChainedOperators) {
+	return function (...operators) {
+		return function (value) {
+			return applyChainedOperators([...operators], value);
+		};
+	};
+}
+
+function applyChainedOperators(operators, value) {
+	let result = value;
+	operators.forEach((operator) => {
+		result = operator(result);
+	});
+	return result;
+}
+
+let pipe = curryArithmetic(applyChainedOperators);
+
+console.log("Expected 3, Result :", applyChainedOperators([add1, mul2, div4, add1], 3));
+console.log("Expected 2, Result :", pipe(add1, mul2, div4)(3));
+console.log("Expected 1.75, Result :", pipe(mul2, add1, div4)(3));
+console.log("Expected 2.5, Result :", pipe(mul2, div4, add1)(3));
+console.log("Expected 5, Result :", pipe(mul2, div4, add1, mul2)(3));
+
