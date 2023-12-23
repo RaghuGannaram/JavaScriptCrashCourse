9095dc135109b07975e337a9f7b0f306f394c280
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-12-23 15:49:40 +0530

    Updated Core JavaScript/currying

--- Core JavaScript/currying.js
+++ Core JavaScript/currying.js
@@ -1,30 +1,32 @@
-let add1 = (x) => x + 1;
-let mul2 = (x) => x * 2;
-let div4 = (x) => x / 4;
+/*
+    Currying in JavaScript is a functional programming technique that transforms a function with multiple arguments into a sequence of functions, 
+    each taking a single argument.
+*/
+//----------------------------------------------------------currying with bind------------------------------------------------------------
+function add(a, b) {
+    return a + b;
+}
 
-function curryArithmetic(func) {
-    return function (...operators) {
-        return function (value) {
-            return func([...operators], value);
-        };
+const add5 = add.bind(this, 5);
+const add10 = add.bind(this, 10);
+
+console.log(add5(1));
+console.log(add10(1));
+
+//----------------------------------------------------------currying with closure------------------------------------------------------------
+function multiply(a) {
+    return function (b) {
+        return a * b;
     };
 }
 
-function applyChainedOperators(operators, value) {
-    let result = value;
-    operators.forEach((operator) => {
-        result = operator(result);
-    });
-    return result;
-}
-console.log("Expected 3, Result :", applyChainedOperators([add1, mul2, div4, add1], 3));
+const multiplyBy5 = multiply(5);
+const multiplyBy10 = multiply(10);
 
-let pipe = curryArithmetic(applyChainedOperators);
-console.log("Expected 2, Result :", pipe(add1, mul2, div4)(3));
-console.log("Expected 1.75, Result :", pipe(mul2, add1, div4)(3));
-console.log("Expected 2.5, Result :", pipe(mul2, div4, add1)(3));
-console.log("Expected 5, Result :", pipe(mul2, div4, add1, mul2)(3));
+console.log(multiplyBy5(2));
+console.log(multiplyBy10(2));
 
+//----------------------------------------------------------------Examples--------------------------------------------------------------------
 function logger(date, message) {
     return `Date : ${date} , Message : ${message}`;
 }
@@ -75,14 +77,46 @@ console.log(curriedSum(1)(2));
 let infiniteCurriesSum = infiniteCurry(sum);
 console.log(infiniteCurriesSum(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)(11)(12)());
 
-//----------------------------------------------------------function composition------------------------------------------------------------
+//---------------------------------------------------------Function Composition----------------------------------------------------------
 const compose = function (functions) {
     return function (x) {
-        return functions.reduceRight((val, fn) => fn(val), x);
-        // return functions.reduce((val, fn) => fn(val), x);
+        // return functions.reduceRight((val, fn) => fn(val), x);
+        return functions.reduce((val, fn) => fn(val), x);
     };
 };
 
-const encrypter = compose([(x) => x + 5, (x) => x * 10, (x) => x ** 2]);
+const encrypt = compose([(x) => x + 1, (x) => x * 5, (x) => x ** 2]);
 
-console.log(encrypter(2));
+console.log(encrypt(2));
+
+//--------------------------------------------------- Currying with Function Chaining----------------------------------------------------
+
+let add1 = (x) => x + 1;
+let mul5 = (x) => x * 5;
+let pow2 = (x) => x ** 2;
+
+function applyOperations(operations, value) {
+    return operations.reduce((currentValue, operation) => operation(currentValue), value);
+}
+
+function createPipeline(...operations) {
+    return function (value) {
+        return applyOperations(operations, value);
+    };
+}
+
+// Using the curried function to create pipelines
+const pipeline1 = createPipeline(add1, mul5, pow2);
+console.log("Result:", pipeline1(2));
+
+const pipeline2 = createPipeline(add1, mul5, pow2);
+console.log("Result:", pipeline2(3));
+
+const pipeline3 = createPipeline(mul5, pow2, mul5);
+console.log("Result:", pipeline3(3));
+
+const pipeline4 = createPipeline(mul5, pow2, add1);
+console.log("Result:", pipeline4(3));
+
+const pipeline5 = createPipeline(mul5, pow2, add1, mul5);
+console.log("Result:", pipeline5(3));
