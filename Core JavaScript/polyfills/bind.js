eae25dfc309c29edb15dcaffaf891ed2f80360b2
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-12-23 15:54:29 +0530

    Updated bind polyfill

--- Core JavaScript/polyfills/bind.js
+++ Core JavaScript/polyfills/bind.js
@@ -1,8 +1,32 @@
+let employee = {
+    getInfo: function (company, salary) {
+        return `|name: ${this.firstName} ${this.lastName} \n| company: ${company} \n| salary: ${salary}`;
+    },
+};
+
+let employee1 = {
+    firstName: "John",
+    lastName: "Doe",
+};
+
+let employee2 = {
+    firstName: "Jane",
+    lastName: "Doe",
+};
+
+Function.prototype.bind = null;
+
 if (!Function.prototype.bind) {
-    Function.prototype.bind = function(context, ...args) {
+    Function.prototype.bind = function (context, ...args1) {
         var fn = this;
-        return function() {
-            return fn.apply(context, args.concat(Array.prototype.slice.call(arguments)));
+        return function (...args2) {
+            return fn.apply(context, [...args1, ...args2]);
         };
     };
 }
+
+let googleEmployeeInfo = employee.getInfo.bind(employee1, "Google");
+let faceBookEmployeeInfo = employee.getInfo.bind(employee2, "Facebook");
+
+console.log(googleEmployeeInfo("$100k"));
+console.log(faceBookEmployeeInfo("$200k"));
